18f3b98314cca6182beb452f17e7e52cde5409de
Daniel Lu <daniel@lawrence.lu>
2024-04-02 13:41:12 -0400

    rename env var

--- add_roles.py
+++ add_roles.py
@@ -96,6 +96,6 @@ async def on_ready():
     print(f"Logged in as {bot.user.name}")
 
 
-token = os.getenv("DISCORD_TOKEN")
+token = os.getenv("APOBOT_TOKEN")
 print("Discord token:", token)
 bot.run(token)
